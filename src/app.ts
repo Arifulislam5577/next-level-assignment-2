54bf43a38c8c904049d11fc498aab51099c5251a
Arifulislam5577 <arifalamm33@gmail.com>
2024-05-22 10:08:18 +0600

    Removed: Morgan package removed.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import express, { Application, Request, Response } from 'express'
-import morgan from 'morgan'
-import config from './config'
+import orderRoutes from './modules/orders/order.route'
 import productRoutes from './modules/product/product.route'
 
 const app: Application = express()
 
-if (config.NODE_ENV === 'DEVELOPMENT') {
-  app.use(morgan('tiny'))
-}
-
 app.use(express.json())
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/orders', orderRoutes)
 
 export default app
